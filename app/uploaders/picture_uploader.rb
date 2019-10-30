31c79e0521143a9bdee50befacea83b82a26d4e7
txcany <akdonnelly@gmail.com>
2019-10-30 13:21:12 +0000

    style added

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,6 +4,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+if Rails.env.production?
   storage :aws
 else
   storage :file
